8acf4050b885ca937540b66aede1575f748262b0
petxcode <brighterdayscodelab@gmail.com>
2024-10-29 07:59:38 +0100

    pipeline

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+
+const port: number = 1144;
+const app: Application = express();
+
+app.use(express.json());
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
